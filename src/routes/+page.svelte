7db2cdf4be8641f8bbc76d9e052f4ea85514674b
PixelParallax <abombsc@gmail.com>
2024-05-12 23:44:01 -0400

    project init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+
+<script>
+    // @ts-ignore
+    import Calendar from '@event-calendar/core';
+    // @ts-ignore
+    import TimeGrid from '@event-calendar/time-grid';
+    // @ts-ignore
+    import DayGrid from '@event-calendar/day-grid';
+    // @ts-ignore
+    import List from '@event-calendar/list';
+
+    let plugins = [TimeGrid, DayGrid, List];
+    let options = {
+        view: 'timeGridWeek', 
+        events: [
+            // your list of events
+        ]
+    };
+</script>
+
+<Calendar {plugins} {options} />
+<h1 class="text-slate-800">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
