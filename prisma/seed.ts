a4469caddb9e6203be4f7408758fee2c447c0065
PixelParallax <abombsc@gmail.com>
2024-05-14 23:07:13 -0400

    seed script progress

---
+++ prisma/seed.ts
@@ -0,0 +1,63 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+
+    // Create a company
+    const company = await prisma.company.create({
+        data: {
+            name: 'Test Company',
+            login: 'testcompany',
+        }
+    })
+
+    console.log('Created company with id:', company.id)
+
+    // Create an employee for the company
+    const employee = await prisma.employee.create({
+        data: {
+            firstName: 'Test',
+            lastName: 'Employee',
+            companyId: company.id
+        }
+    })
+
+    console.log('Created employee with id:', employee.id)
+
+    // Create a job for the company and assign the employee to it
+    const job = await prisma.job.create({
+        data: {
+            title: 'Make coffee',
+            companyId: company.id,
+            employeeIds: [employee.id],
+            hours: 30
+        }
+    })
+
+    console.log('Created job with id:', job.id)
+
+    // Create a calander event for the job
+    await prisma.event.create({
+        data: {
+            title: 'Make coffee',
+            date: new Date(),
+            color: 'red',
+            jobId: job.id,
+            companyId: company.id,
+            employeeId: employee.id
+        }
+    })
+
+    console.log('Created event')
+}
+
+// Run the seed function
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
